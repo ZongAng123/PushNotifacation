d292a98d8e695f3bac5a989121ec9cce8ef2c206
ZongAng123 <zongang15@yahoo.com>
2017-03-03 14:15:30 +0800

    PushNotifacation
    PushNotifacation

--- PushNotifacation/AppDelegate.m
+++ PushNotifacation/AppDelegate.m
@@ -16,30 +16,69 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // 在应用程序启动后覆盖自定义点.
+    [self registerNotifa]; // 1、注册通知
+    if (launchOptions[UIApplicationLaunchOptionsLocalNotificationKey]) { // 用户通过通知(App未运行,已经杀死)打开程序,这里通过 Badge 就可以观察到
+        application.applicationIconBadgeNumber=10;
+        UILocalNotification *myLocalNotifa=launchOptions[UIApplicationLaunchOptionsLocalNotificationKey]; // 返回的是本地通知的对象
+        NSDictionary *dicValue=myLocalNotifa.userInfo;
+        NSString *detailMess=dicValue[@"detailMess"];
+        self.myMess=[NSString stringWithFormat:@"%@(通过通知(App未运行,已经杀死)打开程序,这里是具体的通知信息详情)",detailMess];
+    }
+    else{  // 用户通过App的图片打开的程序
+        NSLog(@"用户通过App的图标打开程序");
+        application.applicationIconBadgeNumber=0;
+        
+    }
     return YES;
+}
+#pragma mark 注册用户通知 ／／1、
+-(void)registerNotifa{
+    // 因为这里系统大于IOS 8.0,所有不需要考虑IOS 8.0 以下
+    
+    UIUserNotificationType notifaType=UIUserNotificationTypeAlert|UIUserNotificationTypeSound|UIUserNotificationTypeBadge;
+    UIUserNotificationSettings *userNotifi=[UIUserNotificationSettings settingsForTypes:notifaType categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:userNotifi];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
+#pragma mark -通知有关的代理方法
+/** App在后台,程序未被杀死,用户点击了本地通知后的操作 */
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
+    
+    // 一定要判断App是否在后台
+    if (!(application.applicationState==UIApplicationStateActive)) { // App在后台且用户点击了通知后的操作)
+        application.applicationIconBadgeNumber=0;
+        NSString *strValue=notification.userInfo[@"detailMess"]; // 这里的 userInfo 对应通知的设置信息 userInfo
+        self.myMess=[NSString stringWithFormat:@"%@(App在后台且用户点击了通知后,这里是具体的通知信息详情)",strValue];
+        
+        [[NSNotificationCenter defaultCenter]postNotificationName:@"bgckMess" object:nil userInfo:nil];
+        NSLog(@"App还在后台,点击了本地通知后,进入这个方法.得到的本地其他信息:%@",notification.userInfo);
+    }
+    else{
+        NSLog(@"App在前台,不用做任何操作");
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // 当应用程序即将从活动状态转换为非活动状态时发送。 这可能发生在某些类型的临时中断（例如来电或SMS消息）或用户退出应用程序时，并且它开始转换到后台状态.
+    // 使用此方法暂停正在进行的任务，禁用计时器，并降低OpenGL ES帧速率。 游戏应该使用这种方法暂停游戏.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    // 使用此方法释放共享资源，保存用户数据，使计时器无效，并存储足够的应用程序状态信息以将应用程序恢复到其当前状态（如果稍后终止）.
+    // 如果您的应用程序支持后台执行，则调用此方法，而不是applicationWillTerminate：当用户退出时.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    // 被称为从后台到非活动状态的转换的一部分; 在这里你可以撤消许多在输入背景所做的更改.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // 在应用程序处于非活动状态时，重新启动已暂停（或尚未启动）的任何任务。 如果应用程序以前在后台，则可选择刷新用户界面.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // 在应用程序即将终止时调用。 保存数据（如果适用）。 另请参见applicationDidEnterBackground：.
 }
 
 @end
--- PushNotifacation/ViewController.m
+++ PushNotifacation/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UILabel * myMessWithLabel;
+@property (nonatomic, strong) UIButton * button12;
 @end
 
 @implementation ViewController
@@ -17,6 +19,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.myMessWithLabel=[[UILabel alloc]initWithFrame:CGRectMake(30, 345, 540, 18)];
+    [self.view addSubview:_myMessWithLabel];
+    self.button12 =[[UIButton alloc]initWithFrame:CGRectMake(241, 285, 119, 30)];
+    self.button12.backgroundColor =[UIColor redColor];
+    [self.button12 setTitle:@"发送本地通知" forState:UIControlStateNormal];
+    [self.button12 addTarget:self action:@selector(pushBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_button12];
+    
+    [self mySetDatailMess];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(backgroundMessOpeartion) name:@"bgckMess" object:nil];
+}
+-(void)pushBtn:(UIButton *)sender{
+    UILocalNotification *localNotifa=[[UILocalNotification alloc]init];
+    
+    localNotifa.alertBody=@"重大消息:微软把诺基亚品牌卖给了富士康!"; // 通知的内容
+    localNotifa.soundName=@"buyao.wav"; //通知的声音
+    localNotifa.alertAction=@":查看最新重大新闻"; // 锁屏的时候 相当于 滑动来::查看最新重大新闻
+    //    localNotifa.alertTitle=@"弹出标题,我在这里";
+    localNotifa.applicationIconBadgeNumber=3;
+    localNotifa.fireDate=[NSDate dateWithTimeIntervalSinceNow:5.0]; // 触发通知的时间(5秒后发送通知)
+    localNotifa.timeZone=[NSTimeZone defaultTimeZone];  // 设置时区
+    localNotifa.repeatInterval=NSCalendarUnitDay; // 通知重复提示的单位，可以是天、周、月
+    
+    localNotifa.userInfo=@{@"detailMess":@"在移动业务一直没有起色的情况下,微软又要有所动作了,不过这次受害的不是Windows Phone,而是一直多灾多难的诺基亚。"};
+    
+    [[UIApplication sharedApplication]scheduleLocalNotification:localNotifa]; // 调度通知(启动通知)
+}
+-(void)mySetDatailMess{
+    AppDelegate *appdele=(AppDelegate *)[[UIApplication sharedApplication]delegate];
+    self.myMessWithLabel.text=appdele.myMess;
+}
+#pragma mark 通知设置值
+-(void)backgroundMessOpeartion{
+    [self mySetDatailMess];
 }
 
 - (void)didReceiveMemoryWarning {
